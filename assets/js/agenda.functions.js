ed9bc4abe7497433df2aedb4aff18706b04e37bf
Gabriel Souza <gabrielspereira2604@gmail.com>
2020-08-11 11:39:54 -0300

    Changing Schedule

--- assets/js/agenda.functions.js
+++ assets/js/agenda.functions.js
@@ -1,46 +1,86 @@
+$('#registerModal').modal('hide');
+$('#phone').mask('(00) 90000-0000');
+
 const calendarEl = document.getElementById('calendar');
 
-            const calendar = new FullCalendar.Calendar(calendarEl, {
-                plugins: ['interaction', 'dayGrid', 'timeGrid'],
-                defaultView: 'dayGridMonth',
-                header: {
-                    left: 'prev,next,today, timeGridWeek,dayGridMonth',
-                    right: 'title',
-                },
-                locale: 'pt-br',
-                timeZone: 'BRT',        
-                slotDuration: '00:15',
-                minTime: '07:00',
-                maxTime: '22:00',
-                nowIndicator: true,
-                selectable: true,
-                editable: true,
-                navLinks: true, // can click day/week names to navigate views
-                eventLimit: true, // allow "more" link when too many events
+const calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: ['interaction', 'dayGrid', 'timeGrid'],
+    defaultView: 'dayGridWeek',
+    header: {
+        left: 'prev,next,today',
+        right: 'title',
+    },
+    locale: 'pt-br',
+    timeFormat: 'H(:mm)',
+    timeZone: 'BRT',     
+    selectable: true,
+    editable: true,
+    navLinks: true, // can click day/week names to navigate views
+    eventLimit: true, // allow "more" link when too many events
+    contentHeight: 100,
+    // events: {
+    //     url: 'REQUEST_FILE',       
+    // },
+    select: function(info) {            
+        // Evento disparado ao selecionar um dia
+        // $('#registerModal #start').val(info.start.toLocaleString());
+        $('#registerModal #end').val(info.end.toLocaleString());
+        $('#registerModal').modal('show');
+    },
+    eventDrop: function (info) {     
+        //evento disparado ao arrastar o evento (update de data)         
+    },
+    eventResize: function (info) {                
+        //evento disparado ao redimencionar o evento (update de data)        
+    },
+    eventClick: function (info) {    
+        //evento disparado ao selecionar um evento (abre o modal de update)        
+    },
+});
 
-                businessHours: {
-                    // days of week. an array of zero-based day of week integers (0=Sunday)
-                    daysOfWeek: [1, 2, 3, 4, 5, 6],
-                    startTime: '09:00', // a start time (10am in this example)
-                    endTime: '18:00', // an end time (6pm in this example)
-                },              
-                events: {
-                    url: 'REQUEST_FILE',       
-                },
-                select: function(info) {            
-                    // Evento disparado ao selecionar um dia
-                    alert(info.startStr);
-                    console.log(info);
-                },
-                eventDrop: function (info) {     
-                    //evento disparado ao arrastar o evento (update de data)         
-                },
-                eventResize: function (info) {                
-                    //evento disparado ao redimencionar o evento (update de data)        
-                },
-                eventClick: function (info) {    
-                    //evento disparado ao selecionar um evento (abre o modal de update)        
-                },
-            });
+calendar.render();
+
+$('#registerModal').on('hidden.bs.modal', function (e) {
+    $('#registerModal input').val('');
+    $("select[name='estado0']").val(0);
+    $("select[name='estado1']").val(0);
+    $("select[name='cidade0']").html('<option value="" id="selecionar0">Selecione um estado</option>');
+    $("select[name='cidade1']").html('<option value="" id="selecionar0">Selecione um estado</option>');
+    $('#registerModal textarea').val('');
+});
+
+$("select[name='estado0']").change(function () {
+    if ($(this).val()) {
+        $.getJSON('https://servicodados.ibge.gov.br/api/v1/localidades/estados/'+$(this).find("option:selected").attr('data-id')+'/municipios', {id: $(this).find("option:selected").attr('data-id')}, function (json) {
             
-            calendar.render();
+            $("#selecionar0").hide();
+            var options = null;
+            for (var i = 0; i < json.length; i++) {
+                options += '<option value="' + json[i].nome + '" >' + json[i].nome + '</option>';
+            }
+            $("select[name='cidade0']").html(options);
+
+        });
+
+    } else {
+        $("select[name='cidade0']").html('<option value="" id="selecionar0">Selecione um estado</option>');
+    }
+});
+
+$("select[name='estado1']").change(function () {
+    if ($(this).val()) {
+        $.getJSON('https://servicodados.ibge.gov.br/api/v1/localidades/estados/'+$(this).find("option:selected").attr('data-id')+'/municipios', {id: $(this).find("option:selected").attr('data-id')}, function (json) {
+            
+            $("#selecionar1").hide();
+            var options = null;
+            for (var i = 0; i < json.length; i++) {
+                options += '<option value="' + json[i].nome + '" >' + json[i].nome + '</option>';
+            }
+            $("select[name='cidade1']").html(options);
+
+        });
+
+    } else {
+        $("select[name='cidade1']").html('<option value="" id="selecionar1">Selecione um estado</option>');
+    }
+});
