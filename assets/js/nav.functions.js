d07ca698d8e6ad4a699a87e6babc87664baf92c2
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-11-02 19:14:41 -0300

    Fix: Changing navbar style

--- assets/js/nav.functions.js
+++ assets/js/nav.functions.js
@@ -1,32 +1,9 @@
-let state = 'minimized';
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
 
-function toggleNav() {
-  const navElement = document.querySelector('nav.menu');
-  const anchorElements = document.querySelectorAll('nav.menu a');
-
-  if (state === 'minimized') {
-    navElement.classList.remove('minimized');
-    navElement.classList.add('maximized');
-    
-    anchorElements.forEach(item => {
-      item.classList.remove('minimized');
-      item.classList.add('maximized');
-    });
-
-    state = 'maximized';
-
-    return;
-  }
-
-  navElement.classList.remove('maximized');
-  navElement.classList.add('minimized');  
-  
-  anchorElements.forEach(item => {
-    item.classList.remove('maximized');
-    item.classList.add('minimized');
-  });
-
-  state = 'minimized';
-
-  return;
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
